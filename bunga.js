6b72ea322f43f49e6e4b65f160f4c1df83fbf3d7
Kipin.Kipon <89014532+MalvinMixamo@users.noreply.github.com>
2024-11-10 08:38:22 +0700

    Update bunga.js

--- bunga.js
+++ bunga.js
@@ -3,6 +3,6 @@ onload = () =>{
 };
 function kirimWhatsApp(){
     const nomorHP = "628812430700"; // ganti dengan nomor HP kamu dalam format internasional (gunakan kode negara, misal 62 untuk Indonesia)
-    const pesan = "Halo, saya ingin bertanya tentang produk Anda."; // bisa disesuaikan dengan pesan default
+    const pesan = "Mwehehehe, makasih ya"; // bisa disesuaikan dengan pesan default
     window.location.href = `https://wa.me/${nomorHP}?text=${encodeURIComponent(pesan)}`;
 }
